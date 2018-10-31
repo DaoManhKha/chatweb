da835b451bfdee84baf00d7c9e9295bfc722821e
thongvu96 <40567483+thongvu96@users.noreply.github.com>
2018-10-31 15:08:55 +0700

    end header

--- FrontEnd/js/myjs.js
+++ FrontEnd/js/myjs.js
@@ -1,21 +1,30 @@
 $(document).ready(function(){
-		showMenu();
-	});
-	function showMenu(){
+	showMenu();
+	showOption();
+});
+function showOption(){
 		// var showMenu = $('#showMenu');
-		var menu = $('div > ul');
-		$('#showMenu').on('click', function(e){
-			e.preventDefault();
-        	menu.slideToggle();
-		});
-		$(window).resize(function(){
-    		var w = $(window).width();
-    		if(w > 266 && menu.is(':hidden')) {
-        		menu.removeAttr('style');
-    		}
-		});
+		var menuOption = document.getElementById('status-child');
+		if(menuOption.style.display == "block") { // if is menuBox displayed, hide it
+			menuOption.style.display = "none";
+		}
+  		else { // if is menuBox hidden, display it
+  			menuOption.style.display = "block";
+  		}
 		// $('#showMenu').on('click', function(){
 		// 	$(".left").css({"display": "block"});
 		// 	$(".right").css({"display": "block"});
 		// })
+	}
+
+	function showMenu(){
+		// var showMenu = $('#showMenu');
+		var menu = document.getElementById('menu-item');
+
+		if(menu.style.display == "block") { // if is menuBox displayed, hide it
+			menu.style.display = "none";
+		}
+  		else { // if is menuBox hidden, display it
+  			menu.style.display = "block";
+  		}
 	}
